94969812d59f6c1a5068dcf6d62b4af61614b6e2
alexandrefnas <alexandrefnas@gmail.com>
2024-10-10 20:50:35 -0300

    Criação de API Agenda

---
+++ Entities/Contato.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuloAPI.Entities
+{
+	public class Contato
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string MyProperty { get; set; }
+	}
+}
